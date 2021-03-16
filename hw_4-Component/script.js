520a835e2c16cfa7a52e8b3246fe1520f1742462
Vitalii <meizu1998@gmail.com>
2021-03-16 20:47:00 +0200

    refactor button

--- hw_4-Component/script.js
+++ hw_4-Component/script.js
@@ -13,7 +13,6 @@ async function getApi (){
 };
 getApi()
 
-
 function createContent(data){
     let count 
     if(localStorage.getItem('notificationId') !==null){
@@ -30,12 +29,12 @@ function createContent(data){
                 <h3 class="contentTitle">${data[count].title}</h3>
                 <p class="content-discription">${data[count].phrase}</p>
             </div>
-            <img class="close" tabindex="1" src="https://cdn1.iconfinder.com/data/icons/flat-web-browser/100/close-512.png">
+            <input type="image" class="close" src="https://cdn1.iconfinder.com/data/icons/flat-web-browser/100/close-512.png">
         </div>
         <div class="choosing">
-            <img class="button-left" tabindex="3" src="https://icon-library.com/images/previous-next-icon/previous-next-icon-18.jpg">
+        <input class="button-left" type="image" src="https://icon-library.com/images/previous-next-icon/previous-next-icon-18.jpg">
             ${data.map(el=>`<input class="breakPoint" type="radio" name="choosingNotification" id="${el.id}" checked="${count -1}">`).join(' ')}
-            <img class="button-right" tabindex="4" src="https://icon-library.com/images/next-icon-png/next-icon-png-20.jpg">
+        <input class="button-right" type="image" src="https://icon-library.com/images/next-icon-png/next-icon-png-20.jpg">
         </div>
         <div class="block-checkbox">       
                 <input class="check" type="checkbox" tabindex="2">
@@ -57,8 +56,6 @@ function notification(count,data){
         breakPoint[count].checked = true;
 }
 
-
-
 function createNotification(data,count){
     let notificationBlock = document.querySelector('.notification')
     let btnRight = document.querySelector('.button-right');
@@ -75,28 +72,10 @@ function createNotification(data,count){
         }
     })
 
-    btnLeft.addEventListener('keydown',(event)=>{
-        if(event.keyCode === 13){
-            if(count > 0){
-                count--
-                notification(count,data)
-                }
-        }
-    })
-
     btnRight.addEventListener('click',()=>{
         if(count < data.length -1 ){
             count++
             notification(count,data) 
-        }
-    })
-
-    btnRight.addEventListener('keydown',(event)=>{
-        if(event.keyCode === 13){
-            if(count < data.length -1 ){
-                count++
-                notification(count,data) 
-            } 
         }
     })
 
@@ -107,24 +86,10 @@ function createNotification(data,count){
         }
     })
 
-    checkNotification.addEventListener('keydown',function(event){
-        if(event.keyCode === 13){
-            this.checked = true
-            if(this.checked){
-                localStorage.setItem('notificationId',count)
-            }
-        }
-    })
-
     close.addEventListener('click',()=>{
         notificationBlock.remove()
     })
 
-    close.addEventListener('keydown',(event)=>{
-        if(event.keyCode === 13){
-            notificationBlock.remove()
-        }
-    })
 }
 
 
